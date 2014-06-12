f3539de38c3b6414c2b5fc92ab3608d9d5839a9c
Mateusz <mgala@student.tgm.ac.at>
2014-06-12 20:59:43 +0200

    1commit

---
+++ iwas/Test/src/com/example/test/SecondActivity.java
@@ -0,0 +1,83 @@
+package com.example.test;
+
+import java.util.Locale;
+
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.os.Build;
+
+public class SecondActivity extends ActionBarActivity implements OnInitListener{
+	
+	private TextToSpeech tts;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_second);
+
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+		tts = new TextToSpeech(this, this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.second, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_second,
+					container, false);
+			return rootView;
+		}
+	}
+	public void onClick(View v){
+			tts.setLanguage(Locale.GERMAN);
+			EditText et = (EditText) findViewById(R.id.editText1);
+			tts.speak(et.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+			
+	}
+
+	@Override
+	public void onInit(int arg0) {
+		
+		
+	}
+
+}
